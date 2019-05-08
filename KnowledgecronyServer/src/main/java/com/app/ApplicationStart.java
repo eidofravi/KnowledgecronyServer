a4c35184fbf9c68ab5298235d0ffc14b922a2e79
ravi123 <ravi123@192.168.1.8>
2019-05-07 20:14:50 -0400

    deleted angular file accidently added

--- KnowledgecronyServer/src/main/java/com/app/ApplicationStart.java
+++ KnowledgecronyServer/src/main/java/com/app/ApplicationStart.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestMySqlApplication {
+public class ApplicationStart {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringRestMySqlApplication.class, args);
+		SpringApplication.run(ApplicationStart.class, args);
 	}
 }
